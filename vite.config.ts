775d57671b48beaf586daec253dee53516d1c3a6
Luw17 <luisflavio360@gmail.com>
2025-05-26 22:31:39 -0300

    configurando vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,     // permite acesso externo (ex: localhost do host)
+    port: 5173,     // porta padrão usada no Dockerfile e docker-compose
+    strictPort: true // garante falha se a porta já estiver em uso
+  }
 })
