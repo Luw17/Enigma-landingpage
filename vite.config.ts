a3ab871f15ca5a198c59603d47ae8c3c519bd1b2
Luw17 <luisflavio360@gmail.com>
2025-05-26 23:59:18 -0300

    hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,     // permite acesso externo (ex: localhost do host)
-    port: 5173,     // porta padrão usada no Dockerfile e docker-compose
-    strictPort: true // garante falha se a porta já estiver em uso
+    host: true,        // permite acesso externo (ex: localhost do host)
+    port: 5173,        // porta padrão usada no Dockerfile e docker-compose
+    strictPort: true,  // garante falha se a porta já estiver em uso
+    watch: {
+      usePolling: true,  // força o Vite a usar polling para detectar mudanças no container
+      interval: 100      // frequência do polling em ms
+    }
   }
 })
